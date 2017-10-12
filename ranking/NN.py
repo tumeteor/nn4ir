1aaaed35da2bebb872b8cbeb340777537a0c19b6
tumeteor <tumeteor@gmail.com>
2017-10-12 17:12:54 +0200

    nn4ir

---
+++ ranking/NN.py
@@ -0,0 +1,153 @@
+import numpy as np
+import tensorflow as tf
+from util.configs import NNConfig
+from util.data2binvec import DataLoader
+import logging
+
+class NN:
+    def __init__(self):
+        logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s',
+                            datefmt='%d.%m.%Y %I:%M:%S %p', level=logging.INFO)
+
+        self.d_loader = DataLoader()
+        self.input_vector_size = self.d_loader.d_handler.get_vocab_size()
+        self.output_vector_size = self.d_loader.d_handler.get_vocab_size()
+        self.train_dataset, self.train_labels, self.valid_dataset, \
+        self.valid_labels, self.test_dataset, self.test_labels = self.d_loader.get_ttv()
+
+
+    def simpleNN(self):
+        self.log = logging.getLogger("Simple NN")
+        self.log.info("Create the computational graph..")
+        graph = tf.graph()
+        with graph.as_default():
+            with tf.device("/cpu:O"):
+                '''
+                Input data
+                '''
+                tf_train_dataset = None
+                tf_train_labels = None
+                tf_valid_dataset = None
+                tf_test_dataset = None
+
+
+                if NNConfig.regularization:
+                    beta_regu = tf.placeholder(tf.float32)
+
+                if NNConfig.learning_rate_decay:
+                    global_step = tf.Variable(0)
+
+
+                # Variables
+                def init_weights(shape):
+                    return tf.Variable(tf.truncated_normal(shape))
+
+                def init_biases(shape):
+                    return tf.Variable(tf.zeros(shape))
+
+                w_h = init_weights([self.input_vector_size, NNConfig.num_hidden_nodes])
+                b_h = init_biases([self.output_vector_size])
+
+                w_o = init_weights([NNConfig.num_hidden_nodes, self.output_vector_size])
+                b_o = init_biases([self.output_vector_size])
+
+                # Training computation
+
+                def model(dataset, w_h, b_h, w_o, b_o, train_mode):
+                    if NNConfig.dropout and train_mode:
+                        drop_i = tf.nn.dropout(dataset, NNConfig.dropout_keep_prob_input)
+                        h_lay_train = tf.nn.relu(tf.matmul(drop_i, w_h) + b_h)
+                        drop_h = tf.nn.dropout(h_lay_train, NNConfig.dropout_keep_prob_hidden)
+                        return tf.matmul(drop_h, w_o) + b_o
+                    else:
+                        h_lay_train = tf.nn.relu(tf.matmul(dataset, w_h) + b_h)
+                        return tf.matmul(h_lay_train, w_o) + b_o
+
+                logits = model(tf_train_dataset, w_h, b_h, w_o, b_o, True)
+                loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits,
+                                                                              labels=tf_train_labels))
+
+                if NNConfig.regularization:
+                    loss += beta_regu * (tf.nn.l2_loss(w_h) + tf.nn.l2_loss(w_o))
+                if NNConfig.learning_rate_decay:
+                    learning_rate = tf.train.exponential_decay(NNConfig.learning_rate,
+                                                               global_step=global_step,
+                                                               decay_steps=NNConfig.decay_steps,
+                                                               decay_rate=NNConfig.decay_rate,
+                                                               staircase=True)
+                    optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(
+                        loss,global_step=global_step
+                    )
+                else:
+                    optimizer = tf.train.GradientDescentOptimizer(NNConfig.learning_rate).minimize(loss)
+
+                train_prediction = tf.nn.softmax(logits=logits)
+                valid_prediction = tf.nn.softmax(model(tf_valid_dataset, w_h, b_h, w_o, b_o, train_mode=False))
+                test_prediction = tf.nn.softmax(model(tf_test_dataset, w_h, b_h, w_o, b_o, train_mode=False))
+
+
+                with tf.name_scope('accuracy'):
+                    pre = tf.placeholder("float", shape=[None, self.output_vector_size])
+                    lbl = tf.placeholder("float", shape=[None, self.output_vector_size])
+                    accuracy = tf.reduce_mean(tf.cast(tf.nn.sigmoid_cross_entropy_with_logits(pre,lbl), "float"))
+
+            self.log.info("running the session..")
+
+            with tf.Session(graph=graph, config=tf.ConfigProto(log_device_placement=True)) as session:
+                tf.initialize_all_variables().run()
+                self.log.info("Initialized")
+                for step in range(NNConfig.num_steps):
+                    offset = (step * NNConfig.batch_size) % (self.train_labels.shape[0] - NNConfig.batch_size)
+                    batch_data = self.train_dataset[offset:(offset + NNConfig.batch_size), :]
+                    batch_labels = self.train_labels[offset:(offset + NNConfig.batch_size), :]
+
+                    feed_dict = {tf_train_dataset: batch_data, tf_train_labels: batch_labels}
+                    if NNConfig.regularization:
+                        feed_dict[beta_regu] = NNConfig.beta_regu
+
+                    _, l, predictions = session.run([optimizer, loss, train_prediction], feed_dict=feed_dict)
+                    if (step % NNConfig.summary_steps == 0):
+                        self.log.info("Minibatch loss at step %d: %f" %(step, l))
+                        self.log.info("Minibatch accuracy: %.3f%%" % session.run(accuracy, feed_dict={pre: predictions,
+                                                                                                      lbl: self.valid_labels}))
+                        self.log.info("Test accuracy: %.3%%" % session.run(accuracy, feed_dict={pre: test_prediction.eval(),
+                                                                                                lbl: self.test_labels}))
+
+
+                self.print_words(test_prediction.eval(), self.test_labels)
+
+    def print_words(self, preds, labels):
+        for pred, label in zip(preds, labels):
+            label_ids = self.d_loader.d_handler.get_ids_from_binary_vector(label)[0]
+            pred_ids = np.argsort(np.negative(pred))[:label_ids.size]
+            # pred_ids = np.argsort(pred)[(-(label_ids.size)):][::-1]
+            # print(label_ids)
+            # print(pred_ids)
+            print(self.d_loader.d_handler.id_list_to_word_list(label_ids), "-->",
+                  self.d_loader.d_handler.id_list_to_word_list(pred_ids))
+            # break
+
+    def get_words(self, vect):
+        ids = self.d_loader.d_handler.get_ids_from_binary_vector(vect)[0]
+        return self.d_loader.d_handler.id_list_to_word_list(ids)
+
+
+if __name__ == '__main__':
+    try:
+        nn = NN()
+        nn.simpleNN()
+        nn.log.info("done..")
+    except Exception as e:
+        nn.log.exception(e)
+        raise
+
+
+
+
+
+
+
+
+
+
+
---
+++ util/datautil.py
@@ -0,0 +1,324 @@
+import codecs
+import fnmatch
+import os
+import random
+import sys
+import re
+from collections import *
+import nltk
+import numpy as np
+import tensorflow as tf
+import six.moves.cPickle as pickle
+import logging
+
+import csv
+
+# for long CSV
+maxInt = sys.maxsize
+decrement = True
+while decrement:
+    # decrease the maxInt value by factor 10
+    # as long as the OverflowError occurs.
+    decrement = False
+    try:
+        csv.field_size_limit(maxInt)
+    except OverflowError:
+        maxInt = int(maxInt / 10)
+        decrement = True
+
+
+class MyError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
+class TextDataHandler:
+    _force_read_input = False
+    _word_to_id = {}
+    _id_to_word = {}
+    _words = []
+    _unknown_terms = set()
+    _unknown_terms_freq = 0
+
+    def __init__(self, all_doc_path, save_dir):
+
+        files = Utilities.recursive_glob(all_doc_path, "*")
+        Utilities.create_file_dir(save_dir)
+        # base_name = os.path.basename(os.path.normpath(all_doc_path))
+        # word_to_id
+        word_to_id_pickle = os.path.join(save_dir, 'word_to_id.pkl')
+        self._word_to_id = self._maybe_pickling(word_to_id_pickle, self._build_dict_from_a_set_of_file, files)
+
+        # id_to_word
+        id_to_word_pickle = os.path.join(save_dir, 'id_to_word.pkl')
+        self._id_to_word = self._maybe_pickling(id_to_word_pickle, self._build_reverse_dict, self._word_to_id)
+
+    def _maybe_pickling(self, pickle_name, func, *func_param):
+        if os.path.exists(pickle_name) and not self._force_read_input:
+            # You may override by setting force=True.
+            print('%s already present - Skipping pickling.' % pickle_name)
+            try:
+                with open(pickle_name, 'rb') as f:
+                    data = pickle.load(f)
+            except Exception as e:
+                print('Unable to process data from', pickle_name, ':', e)
+                raise
+        else:
+            data = func(*func_param)
+            self.pickling(pickle_name, data)
+        return data
+
+    def pickling(self, pickle_file_name, data):
+        print('Pickling %s' % pickle_file_name)
+        try:
+            with open(pickle_file_name, 'wb') as f:
+                pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
+        except Exception as e:
+            logging.error('Unable to save data to', pickle_file_name, ':', e)
+
+    @staticmethod
+    def clean_str(string):
+        """
+        string cleanin
+        """
+        string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+        string = re.sub(r"\'s", " \'s", string)
+        string = re.sub(r"\'ve", " \'ve", string)
+        string = re.sub(r"n\'t", " n\'t", string)
+        string = re.sub(r"\'re", " \'re", string)
+        string = re.sub(r"\'d", " \'d", string)
+        string = re.sub(r"\'ll", " \'ll", string)
+        string = re.sub(r",", " , ", string)
+        string = re.sub(r"!", " ! ", string)
+        string = re.sub(r"\(", " \( ", string)
+        string = re.sub(r"\)", " \) ", string)
+        string = re.sub(r"\?", " \? ", string)
+        string = re.sub(r"\s{2,}", " ", string)
+        # string = re.sub(r"[^A-Za-z0-9]", " ", string)
+        return string.strip().lower()
+
+    def read_words(self, filename):
+        """
+        Tokenization using NLTK
+        """
+        with tf.gfile.GFile(filename, "r") as f:
+            return nltk.word_tokenize(TextDataHandler.clean_str(f.read()))
+
+    def _build_dict_from_a_set_of_file(self, filenames):
+        counter = Counter()
+        file_cnt = len(filenames)
+        cnt = 0
+        for filename in filenames:
+            if cnt % 500 == 0:
+                print(str(cnt) + " out of " + str(file_cnt) + " has been processed")
+            words = self.read_words(filename)
+            counter.update(words)
+            cnt += 1
+        count_pairs = sorted(counter.items(), key=lambda x: (-x[1], x[0]))
+        # if vocab_size == -1:
+        # 	vocab_size = len(count_pairs)
+        # words, _ = list(zip(*count_pairs[:vocab_size]))
+        words, _ = list(zip(*count_pairs))
+        word_to_id = dict(zip(words, range(1, len(words) + 1)))
+        return word_to_id
+
+    def get_vocab_size(self):
+        if not self._word_to_id.values:
+            raise MyError('first build the dictionary...')
+        return len(self._word_to_id) + 1  # unknown
+
+    def truncate_vocab(self, new_vocab_size):
+        if new_vocab_size < 0:
+            print("New Vocab Size:", self.get_vocab_size())
+            return
+        self._word_to_id = OrderedDict(sorted(self._word_to_id.items(), key=lambda item: item[1])[:new_vocab_size - 1])
+        self._id_to_word = self._build_reverse_dict(self._word_to_id)
+        print("New Vocab Size:", self.get_vocab_size())
+
+    def _build_reverse_dict(self, original_dict):
+        if not original_dict:
+            raise MyError('first build the dictionary...')
+        reverse_dict = dict(zip(original_dict.values(), original_dict.keys()))
+        return reverse_dict
+
+    def get_id_of_word(self, word):
+        #     return self._word_to_id.get(word, 0) # dictionary['UNK'] = 0
+        if word in self._word_to_id:
+            return self._word_to_id[word]
+        else:  # dictionary['UNK'] = 0
+            self._unknown_terms.add(word)
+            self._unknown_terms_freq += 1
+            return 0
+
+    def get_word_of_id(self, i):
+        if i == 0:
+            word = 'UNK'
+        elif i in self._id_to_word:
+            word = self._id_to_word[i]
+        else:
+            print(i)
+            raise MyError('unknown_id')
+        return word
+
+    def word_list_to_id_list(self, words):
+        return [self.get_id_of_word(word) for word in words]
+
+    def id_list_to_word_list(self, ids):
+        return [self.get_word_of_id(i) for i in ids]
+
+    def get_vocab_dict(self):
+        return self._word_to_id.copy()
+
+    def get_rev_vocab_dict(self):
+        return self._id_to_word.copy()
+
+    def get_vocab(self):
+        if not self._words:
+            self._words = list(self._word_to_id)
+        return self._words
+
+    def get_one_hot_vector(self, wordIds_vec):
+        vocab_size = self.get_vocab_size()
+        vec = (np.arange(vocab_size) == np.array(wordIds_vec)[:, None]).astype(np.float32)
+        return np.array(vec)
+
+    def get_ids_from_one_hot_vector(self, one_hot_Vec):
+        # to be implimented
+        return None
+
+    def get_binary_vector(self, wordIds_vec):
+        vec = np.zeros(self.get_vocab_size(), dtype=int)
+        inices = list(set(wordIds_vec))
+        vec[inices] = 1
+        return np.array(vec)
+
+    def get_ids_from_binary_vector(self, binary_vector):
+        return np.nonzero(binary_vector)
+
+    def get_freq_vector(self, wordIds_vec):
+        vec = np.zeros(self.get_vocab_size(), dtype=int)
+        counter = Counter(wordIds_vec)
+        vec[list(counter.keys())] = list(counter.values())
+        return np.array(vec)
+
+    def get_ids_from_freq_vector(self, freq_vector):
+        # return [[freq_vector[i]] * i for i in np.nonzero(freq_vector)]
+        return None
+
+
+class BatchCreator4RNN:
+    def __init__(self, tensor, batch_size, seq_length):
+        self.tensor = tensor
+        self.batch_size = batch_size
+        self.seq_length = seq_length
+        self.create_batches()
+        self.pointer = 0
+
+    # abcd -> x:abc y:bcd
+    def create_batches(self):
+        self.num_batches = int(self.tensor.size / (self.batch_size *
+                                                   self.seq_length))
+        if self.num_batches == 0:
+            assert False, "Not enough data. Make seq_length and batch_size small."
+
+        self.tensor = self.tensor[:self.num_batches * self.batch_size * self.seq_length]
+        xdata = self.tensor
+        ydata = np.copy(self.tensor)
+
+        ydata[:-1] = xdata[1:]
+        ydata[-1] = xdata[0]
+        self.x_batches = np.split(xdata.reshape(self.batch_size, -1), self.num_batches, 1)
+        self.y_batches = np.split(ydata.reshape(self.batch_size, -1), self.num_batches, 1)
+
+    def next_batch(self):
+        x, y = self.x_batches[self.pointer], self.y_batches[self.pointer]
+        self.pointer += 1
+        return x, y
+
+    def reset_batch_pointer(self):
+        self.pointer = 0
+
+
+class Retrieval_Data_Util:
+    doc_query_pairs = []
+
+    def __init__(self, runres, qrel):
+        with codecs.open(qrel, "r", encoding='utf-8', errors='ignore') as f:
+            csv_reader = csv.reader(f, delimiter=' ', quoting=csv.QUOTE_NONE)
+            for row in csv_reader:
+                if row[3] == '1':
+                    self.doc_query_pairs.append((row[2], row[0]))
+            f.close()
+        self._runRes = runres
+
+    def get_rel_qd(self):
+        d = [tup[0] for tup in self.doc_query_pairs]
+        q = [tup[1] for tup in self.doc_query_pairs]
+        return d, q
+
+    def get_pseudo_rel_qd(self, top_k):
+        d = []
+        q = []
+        with codecs.open(self._runRes, "r", encoding='utf-8', errors='ignore') as f:
+            csv_reader = csv.reader(f, delimiter=' ', quoting=csv.QUOTE_NONE)
+            for row in csv_reader:
+                if int(row[3]) <= top_k:
+                    d.append(row[2])
+                    q.append(row[0])
+            f.close()
+        return d, q
+
+    def get_label(self, d, q):
+        return (d, q) in self.doc_query_pairs
+
+
+class Utilities():
+    @staticmethod
+    def shufflize(data, label):
+        assert len(data) == len(label)
+        perm = np.random.permutation(len(data))
+        return data[perm], data[perm]
+
+    @staticmethod
+    def recursive_glob(treeroot, pattern):
+        results = []
+        for base, dirs, files in os.walk(treeroot):
+            goodfiles = fnmatch.filter(files, pattern)
+            results.extend(os.path.join(base, f) for f in goodfiles)
+        return results
+
+    @staticmethod
+    def create_file_dir(filename):
+        if not os.path.exists(os.path.dirname(filename)):
+            try:
+                os.makedirs(os.path.dirname(filename))
+            except OSError as exc:  # Guard against race condition
+                if exc.errno != os.errno.EEXIST:
+                    raise
+
+    @staticmethod
+    def sample_distribution(distribution):
+        """Sample one element from a distribution assumed to be an array of normalized
+    probabilities.
+    """
+        r = random.uniform(0, 1)
+        s = 0
+        for i in range(len(distribution)):
+            s += distribution[i]
+            if s >= r:
+                return i
+        return len(distribution) - 1
+
+    @staticmethod
+    def random_distribution(vector_size):
+        """Generate a random column of probabilities."""
+        b = np.random.uniform(0.0, 1.0, size=[1, vector_size])
+        return b / np.sum(b, 1)[:, None]
+
+    @staticmethod
+    def accuracy(predictions, labels):
+        return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
+                / predictions.shape[0])
