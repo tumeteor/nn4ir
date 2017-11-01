6754551c8e910ad752f8a4e3f88fb6601d93f726
tumeteor <tumeteor@gmail.com>
2017-11-01 17:24:11 +0100

    add pretrained word embeddings

--- ranking/NN.py
+++ ranking/NN.py
@@ -12,16 +12,16 @@ class NN:
         :param labels:
         :return:
         '''
-        data_batches = []
-        labels_batches = []
         data_length = len(data)
         steps = int(data_length / batch_size)
+        data_batches = list()
+        labels_batches = list()
         for step in range(0, steps):
             offset = (step * batch_size) % (data_length - batch_size)
             batch_data = data[offset:(offset + batch_size), :]
             batch_labels = labels[offset:(offset + batch_size)]
-            batch_labels = batch_labels.reshape(data_length, 1)
-            data_batches[step] = batch_data
-            labels_batches[step] = batch_labels
+            batch_labels = batch_labels.reshape(batch_size, 1)
+            data_batches.append(batch_data)
+            labels_batches.append(batch_labels)
 
         return steps, data_batches, labels_batches
