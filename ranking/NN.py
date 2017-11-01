ebb27b89d3056fb6fd57f6c19302f6b1daff4cf0
tumeteor <tumeteor@gmail.com>
2017-11-01 17:00:58 +0100

    add pretrained word embeddings

--- ranking/NN.py
+++ ranking/NN.py
@@ -1,18 +1,27 @@
 from abc import abstractmethod
+import tensorflow as tf
 class NN:
     @abstractmethod
     def simple_NN(self,mode):
         pass
 
     def batchData(self, batch_size, data, labels):
+        '''
+        split data into smaller batch by the first tensor dimension
+        :param batch_size:
+        :param data: Tensor
+        :param labels:
+        :return:
+        '''
         steps = len(data) % batch_size
         data_batches = []
         labels_batches = []
+        data_length = tf.shape(data)[0]
         for step in steps:
-            offset = (step * batch_size) % (len(data) - batch_size)
+            offset = (step * batch_size) % (data_length - batch_size)
             batch_data = data[offset:(offset + batch_size), :]
             batch_labels = labels[offset:(offset + batch_size)]
-            batch_labels = batch_labels.reshape(len(batch_labels), 1)
+            batch_labels = batch_labels.reshape(data_length, 1)
             data_batches[step] = batch_data
             labels_batches[step] = batch_labels
 
