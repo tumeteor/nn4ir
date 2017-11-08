7fd729067206041b6658f5b00fe26a0328cda17c
tumeteor <tumeteor@gmail.com>
2017-11-08 16:36:09 +0100

    add pretrained word embeddings

--- Util/configs.py
+++ Util/configs.py
@@ -13,12 +13,12 @@ class NNConfig:
     beta_regu = 1e-3
     dropout_keep_prob_input = 1
     dropout_keep_prob_hidden = 0.5
-    learning_rate = 0.5
+    learning_rate = 0.01
     decay_steps = 1000
     decay_rate = 0.65
     regularization = True
     dropout = True
-    learning_rate_decay = True
+    learning_rate_decay = False
     sampler = None  # uniform, log_uniform, learned_unigram, fixed_unigram
     candidate_sampling = None  # nce_loss, softmax_loss
     num_sampled = 64  # Number of negative examples to sample.
